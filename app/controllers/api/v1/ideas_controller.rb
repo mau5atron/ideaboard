d33a35b2bf165edccc4057f3cc7e7d8bae2466ee
mau5atron <gabrielbetancourt96@outlook.com>
2017-12-16 14:24:25 -0800

    Updated backend

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,37 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
       render json: @ideas
+    end
+
+    def create
+    	@idea = Idea.create(idea_params)
+    	render json: @idea
+    end
+
+    def update
+    	@idea = Idea.find(params[:id])
+    	# find idea by id
+    	@idea.update_attributes(idea_params)
+    	# allow idea to be updated through its strong 
+    	# params 
+    	render json: @idea
+    	# ideas will be rendered in json after being updated.
+    end
+
+    def destroy
+      @idea = Idea.find(params[:id])
+      if @idea.destroy
+        head :no_content, status: :ok
+      else 
+        render json: @idea.errors, status: :unprocessable_entity
+      end
+    end
+
+    private 
+    def idea_params
+    	params.require(:idea).permit(:title, :body)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
       resources :ideas  
     end
   end
+
 end
