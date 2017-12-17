8720a56de20d649f78c62d3661a6be58c7e1a84c
mau5atron <gabrielbetancourt96@outlook.com>
2017-12-16 16:26:48 -0800

    Updated database to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171202001428) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.string "body"
