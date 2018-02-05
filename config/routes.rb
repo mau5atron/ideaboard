485712e663cf82ffc88c3bb6aa3d00308530985d
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-05 13:24:35 -0800

    updated root // updated gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :ideas 
+      resources :ideas
+      get 'api/v1/ideas'
+      root 'api/v1/ideas#index'
     end
   end
 
